3d41c495424d8fbaca384c943ae79fe30e483b3b
Logikable <logikable@users.noreply.github.com>
2026-05-29 21:15:52 -0700

    Add tests for SumEquipStats and LoadTextProto

---
+++ src/equip_stats_test.cc
@@ -0,0 +1,64 @@
+#include "src/equip_stats.h"
+
+#include "gtest/gtest.h"
+#include "src/equip.pb.h"
+
+namespace ms {
+namespace {
+
+TEST(SumEquipStatsTest, EmptyListReturnsZero) {
+  EquipStats result = SumEquipStats({});
+  EXPECT_EQ(result.str(), 0);
+  EXPECT_EQ(result.attack(), 0);
+}
+
+TEST(SumEquipStatsTest, SingleElementIsIdentity) {
+  EquipStats a;
+  a.set_str(5);
+  a.set_attack(10);
+  EquipStats result = SumEquipStats({a});
+  EXPECT_EQ(result.str(), 5);
+  EXPECT_EQ(result.attack(), 10);
+}
+
+TEST(SumEquipStatsTest, AllFieldsAreSummed) {
+  EquipStats a;
+  a.set_str(1);
+  a.set_dex(2);
+  a.set_int_(3);
+  a.set_luk(4);
+  a.set_attack(5);
+  a.set_magic_attack(6);
+  a.set_max_hp(7);
+  a.set_def(8);
+
+  EquipStats b;
+  b.set_str(10);
+  b.set_dex(20);
+  b.set_int_(30);
+  b.set_luk(40);
+  b.set_attack(50);
+  b.set_magic_attack(60);
+  b.set_max_hp(70);
+  b.set_def(80);
+
+  EquipStats result = SumEquipStats({a, b});
+  EXPECT_EQ(result.str(), 11);
+  EXPECT_EQ(result.dex(), 22);
+  EXPECT_EQ(result.int_(), 33);
+  EXPECT_EQ(result.luk(), 44);
+  EXPECT_EQ(result.attack(), 55);
+  EXPECT_EQ(result.magic_attack(), 66);
+  EXPECT_EQ(result.max_hp(), 77);
+  EXPECT_EQ(result.def(), 88);
+}
+
+TEST(SumEquipStatsTest, ThreeElementsAccumulate) {
+  EquipStats a;
+  a.set_attack(3);
+  EquipStats result = SumEquipStats({a, a, a});
+  EXPECT_EQ(result.attack(), 9);
+}
+
+}  // namespace
+}  // namespace ms
